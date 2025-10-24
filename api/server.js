19ed03b13b65a8c939d2f5b1f0e72a7ab7bcc279
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2025-10-23 23:52:54 -0300

    fix: ensure name server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-
 const fs = require("fs");
 const path = require("path");
-const filePath = path.join("path");
+
+const server = jsonServer.create();
+
+const filePath = path.join("db.json");
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
